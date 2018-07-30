dfd2751f00bf358950221f242979221b522ecec4
raphael meyer <raphael.akira.meyer@gmail.com>
2018-07-30 21:14:29 +0200

    set up WIP for first scenario

--- pickle-cpp/src/cucumber.cc
+++ pickle-cpp/src/cucumber.cc
@@ -1,7 +1,5 @@
 #include <pickle-cpp/cucumber.h>
 
-#include <iostream>
-
 namespace cucumber {
 
 void Given(std::string name, std::function<void(Pickle const &)> definition) {
@@ -11,9 +9,6 @@ void When(std::string name, std::function<void(Pickle const &)> definition) {
 }
 
 void Then(std::string name, std::function<void(Pickle const &)> definition) {
-  std::cout
-    << "1 scenario (1 passed)" << "\n"
-    << "1 step (1 passed)" << "\n";
 }
 
 } // namespace cucumber
