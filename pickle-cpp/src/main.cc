ebdb13c0090845f0c3e2a4f6530db1bba004b699
raphael meyer <raphael.akira.meyer@gmail.com>
2018-08-17 17:21:25 +0200

    do something in main

--- pickle-cpp/src/main.cc
+++ pickle-cpp/src/main.cc
@@ -1,3 +1,17 @@
+#include "runner.h"
+
+#include <pickle/os/subprocess.h>
+#include <pickle/os/oserror.h>
 
 int main() {
+  pickle::Runner runner;
+  auto result = pickle::create_subprocess("../cucumber-pickle-runner");
+  if(auto error = std::get_if<pickle::OSError>(&result); error) {
+    std::cerr << error->what << "\n";
+    return 1;
+  }
+
+  auto subprocess = std::get<std::shared_ptr<pickle::Subprocess>>(result);
+  runner.run(subprocess->io());
+  return 0;
 }
